e5a3e355f91cf0ea1c41806ac2980a196a9ef789
Shadaab <Shadaab113@gmail.com>
2024-07-30 14:11:44 +0530

    this my third commit...

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,1 +1,32 @@
-#just update
+# import pytest
+from selenium import webdriver
+from pageObjects.LoginPage import loginPage
+
+class Test_001_Login:
+    baseUrl = "https://admin-demo.nopcommerce.com/"
+    username ="admin@yourstore.com"
+    password = "admin"
+
+    def test_homepageTitle(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.baseUrl)
+        act_title = self.driver.title
+        self.driver.close()
+        if act_title == "Your store. Login":
+            assert True
+        else:
+            assert False
+
+    def test_login(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get(self.baseUrl)
+        self.lp = loginPage(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setPassword(self.password)
+        self.lp.clickLogin()
+        act_title = self.driver.title
+        self.driver.close()
+        if act_title == "Dashboard / nopCommerce administration":
+            assert True
+        else:
+            assert False
