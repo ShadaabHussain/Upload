b98aa4ccbdb36a90f60a3bfb4895997089a7778d
Shadaab <Shadaab113@gmail.com>
2024-08-06 14:05:14 +0530

    this my latest commit...

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -7,26 +7,32 @@ class Test_001_Login:
     username ="admin@yourstore.com"
     password = "admin"
 
-    def test_homepageTitle(self):
-        self.driver = webdriver.Chrome()
+    def test_homepageTitle(self,setup):
+        self.driver = setup
         self.driver.get(self.baseUrl)
         act_title = self.driver.title
-        self.driver.close()
         if act_title == "Your store. Login":
             assert True
+            self.driver.close()
+
         else:
+            self.driver.save_screenshot(".\\Screenshots\\"+"test_homepageTitle.png")
+            self.driver.close()
             assert False
 
-    def test_login(self):
-        self.driver = webdriver.Chrome()
+    def test_login(self,setup):
+        self.driver = setup
         self.driver.get(self.baseUrl)
         self.lp = loginPage(self.driver)
         self.lp.setUserName(self.username)
         self.lp.setPassword(self.password)
         self.lp.clickLogin()
         act_title = self.driver.title
-        self.driver.close()
         if act_title == "Dashboard / nopCommerce administration":
             assert True
+            self.driver.close()
+
         else:
+            self.driver.save_screenshot(".\\Screenshots\\"+"test_login.png")
+            self.driver.close()
             assert False
